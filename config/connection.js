684b9b02126a22731449f133e3c6975490e955f0
ShareeO <selena74354@yahoo.com>
2021-07-11 22:48:22 -0500

    config/connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
 });
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 module.exports = mongoose.connection;
